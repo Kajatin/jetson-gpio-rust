3c92d5fa15cefd8f16caf53bcae621abcf585b85
Roland Kajatin <roland@teton.ai>
2023-01-23 23:11:08 +0100

    added loads of error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
+pub mod gpio;
 pub mod gpio_pin_data;
-// pub mod gpio;
-pub mod gpio_new;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -8,24 +7,23 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
-    // use crate::gpio::{setmode, setup, output, cleanup, Direction, Level};
-    use crate::gpio_new::{GPIO, Direction, Level};
+    use crate::{gpio::{GPIO, Direction, Level}, gpio_pin_data::Mode};
 
     #[test]
     fn it_works() {
         let mut gpio = GPIO::new();
-        gpio.setmode(String::from("BOARD"));
+        gpio.setmode(Mode::BOARD).unwrap();
 
         // ERROR line 344: mutex lock() blocks -> double lock...
-        gpio.setup(vec![7, 11], Direction::OUT, Some(Level::LOW));
+        gpio.setup(vec![7, 11], Direction::OUT, Some(Level::LOW)).unwrap();
 
         for _ in 0..3 {
                 std::thread::sleep(std::time::Duration::from_secs(1));
-                gpio.output(vec![7, 11], vec![Level::HIGH, Level::HIGH]);
+                gpio.output(vec![7, 11], vec![Level::HIGH, Level::HIGH]).unwrap();
                 std::thread::sleep(std::time::Duration::from_secs(1));
-                gpio.output(vec![7, 11], vec![Level::LOW, Level::LOW]);
+                gpio.output(vec![7, 11], vec![Level::LOW, Level::LOW]).unwrap();
             }
 
-            gpio.cleanup(None);
+            gpio.cleanup(None).unwrap();
     }
 }
