e351c0d8bb71fa17ee4f95a4d72f8ff19824cf18
Roland Kajatin <roland.kajatin@gmail.com>
2023-01-23 21:28:02 +0100

    adding struct based lib code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod gpio_pin_data;
-pub mod gpio;
+// pub mod gpio;
+pub mod gpio_new;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -7,22 +8,24 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use crate::gpio::{setmode, setup, output, cleanup, Direction, Level};
+    // use crate::gpio::{setmode, setup, output, cleanup, Direction, Level};
+    use crate::gpio_new::{GPIO, Direction, Level};
 
     #[test]
     fn it_works() {
-        setmode(String::from("BOARD"));
+        let mut gpio = GPIO::new();
+        gpio.setmode(String::from("BOARD"));
 
         // ERROR line 344: mutex lock() blocks -> double lock...
-        setup(vec![7, 11], Direction::OUT, Some(Level::LOW));
+        gpio.setup(vec![7, 11], Direction::OUT, Some(Level::LOW));
 
         for _ in 0..3 {
                 std::thread::sleep(std::time::Duration::from_secs(1));
-                output(vec![7, 11], vec![Level::HIGH, Level::HIGH]);
+                gpio.output(vec![7, 11], vec![Level::HIGH, Level::HIGH]);
                 std::thread::sleep(std::time::Duration::from_secs(1));
-                output(vec![7, 11], vec![Level::LOW, Level::LOW]);
+                gpio.output(vec![7, 11], vec![Level::LOW, Level::LOW]);
             }
 
-            cleanup(None);
+            gpio.cleanup(None);
     }
 }
